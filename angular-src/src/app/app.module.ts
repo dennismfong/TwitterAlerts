74e81deac2041b6bba263b112736aacf68e08feb
dennismfong <dfong2014@gmail.com>
2017-09-22 12:02:36 -0700

    Connecting form front end to register user to backend

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -13,7 +13,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 import { ValidateService} from './services/validate.service';
-import { FlashMessagesModule} from 'angular2-flash-messages';
+import { AuthService } from './services/auth.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 const appRoutes: Routes = [
   {path:'', component: HomeComponent},
@@ -41,7 +42,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService],
+  providers: [ValidateService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
